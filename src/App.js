b83ae3407ee9ab26aed961aa65dcd0af1dbf8101
Abhay <99758025+AB1718@users.noreply.github.com>
2024-02-29 09:25:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,12 +25,12 @@ function App() {
         <img src={Img1}></img>
       </div>
       <div>
-      <p>
-      <p className='font-bold'>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</p>
+      <div>
+      <div className='font-bold'>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</div>
        <br/>
       C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.
       C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. 
-      </p>
+      </div>
       <br/>
       <div>
         <img 
